314244f4f6da3f063946fd2ae3af09566eb531bf
Gustavo Warmling <gustavowarmling@gmail.com>
2021-12-22 12:12:14 -0300

    Initial commit 🎆

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiZ3VzdGF2b3dhcm1saW5nIiwiYSI6ImNreGhtdndjazByaGsyeG1sYWp3aW56dTgifQ.NPHYdnWCiaO-JW29xcxxnQ'; // Public token provided by mapbox
 
 export const Home = () => {
-  return(
-    <h1>Home</h1>
-  )
-}
+  let map: mapboxgl.Map;
+
+  useEffect(() => {
+    map = new mapboxgl.Map({
+      container: 'mapWrapper',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-74.5, 40],
+      zoom: 9,
+    });
+  }, []);
+
+  const styleChange = async () => {
+    if (map.getStyle().name !== 'Mapbox Streets') {
+      map.setStyle('mapbox://styles/mapbox/streets-v11');
+    }
+
+    map.setStyle('mapbox://styles/mapbox/satellite-v9');
+  };
+
+  return (
+    <div style={{ height: '100vh' }} id="mapWrapper">
+      <button type="button" onClick={styleChange}>
+        Trocar estilo
+      </button>
+    </div>
+  );
+};
